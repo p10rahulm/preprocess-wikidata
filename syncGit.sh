0fcffbd1efa0f594084220729d7514283a1e98cf
Rahul Madhavan <rahulmadhavan@google.com>
2023-10-12 19:15:22 +0530

    new commit

--- syncGit.sh
+++ syncGit.sh
@@ -1,11 +1,11 @@
-# Git pull
-git pull --rebase origin main
-
 # Add files to git repository
 git add -A
 
 # Create a commit
 git commit -m "new commit"
+
+# Git pull
+git pull --rebase origin main
 
 # Push to remote
 git push origin main
